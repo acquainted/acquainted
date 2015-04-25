2730c742e5234e9ee3574e6e4f61d1fb6b7f0880
Mandy Yeung <mandycodes@gmail.com>
2015-04-25 10:47:05 -0400

    Working mailbox

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,26 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'dashboard#index'
-  resources :conversations, only: [:index, :show, :destroy]
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :reply
+    end
+  end
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :restore
+    end
+  end
+  resources :conversations, only: [:index, :show, :destroy] do
+    collection do
+      delete :empty_trash
+    end
+  end
+  resources :conversations, only: [:index, :show, :destroy] do
+    member do
+      post :mark_as_read
+    end
+  end
   resources :messages, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
