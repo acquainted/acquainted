c76c7092e91c32874939827754df326a48798f24
Mandy Yeung <mandycodes@gmail.com>
2015-04-25 15:46:12 -0400

    Simpleform fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425190350) do
+ActiveRecord::Schema.define(version: 20150425192241) do
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.integer "unsubscriber_id"
@@ -92,6 +92,8 @@ ActiveRecord::Schema.define(version: 20150425190350) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
